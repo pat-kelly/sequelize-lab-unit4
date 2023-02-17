a849be776d8370ec1b682d29467f337ea1fa1607
Patrick <kelly.patj@gmail.com>
2023-02-16 22:22:24 -0500

    create/index functionality

--- controllers/penguins.js
+++ controllers/penguins.js
@@ -1,7 +1,25 @@
 const { Penguin } = require('../models');
 
+const create = async(req, res) =>{
+  try {
+    const peng = await Penguin.create(req.body);
+    res.status(200).json(peng);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+}
 
+const index = async(req, res) =>{
+  try {
+    const pengs = await Penguin.findAll();
+    res.status(200).json(pengs);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+}
 
 module.exports = {
-  
+  create,
+  index,
 }
--- routes/penguins.js
+++ routes/penguins.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const pengCtrl = require('../controllers/penguins.js');
 
-
+router.post('/', pengCtrl.create);
+router.get('/', pengCtrl.index);
 
 
 module.exports = router;
