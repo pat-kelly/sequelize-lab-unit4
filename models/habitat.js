0ccc743ef7f05bbeeffe4791cbc7658720c312eb
Patrick <kelly.patj@gmail.com>
2023-02-16 22:56:19 -0500

    adds associations

--- models/habitat.js
+++ models/habitat.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Habitat.belongsTo(models.Penguin, {
+        foreignKey: 'pengId',
+      })
     }
   }
   Habitat.init({
--- models/penguin.js
+++ models/penguin.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Penguin.hasMany(models.Habitat, {
+        foreignKey: 'pengId',
+        as: 'habitats'
+      })
     }
   }
   Penguin.init({
